86fdb9312860c64b8e10fffea7d0491c20596472
theversatilehobbyist <85900053+theversatilehobbyist@users.noreply.github.com>
2021-07-21 13:56:11 -0700

    Update shoppinglist.py

--- shoppinglist.py
+++ shoppinglist.py
@@ -1,25 +1,44 @@
-# The Shopping List
-list = ['example food', 'example food', 'example food']
+print("Welcome to Shopping List!")
+print("Press 1 to view your shopping list")
+print("Press 2 to add an item into your shopping list")
+print("Press 3 to delete an item from your shopping list")
+print("Press 4 to exit the shopping list")
+print("If your shopping list looks like this : [''], that means it is empty.")
 
-#Commands
-print('Welcome to Your Personalized Shopping List!')
-print('')
-print(' To add an item to the list please just type the word \'add\'')
-print('')
-print(' To check the if a certain item is in the list please just type the word \'check\'')
-print('')
-print(' And to view the entire list please type the word \'view\'')
-print('')
+placeholder = ""
+shoppingList = [placeholder]
 
-#Input
-b = input('Enter the command you would like to give the Shopping List ')
-val = b
-#View Entire List
-if val == 'view' or 'View':
-    print('List Items')
-    for x in list:
-        print(x)
+#While loop
+while True:
+    #Question
+    answer = int(input("What would you like to do? "))
 
-#Or Else
-else:
-    print('Sorry we didn\'t recognize that command')
+    #If the answer is 1 then print out the shoppingList
+    if answer == 1:
+        print(shoppingList)
+
+    elif answer == 2:
+        option = input("Would you like to add one item add a time or several items? Press o for one at a time and s for several items at a time. ")
+        if option == "o":
+            itemadd = input("What would you like to add? ")
+            shoppingList.append(itemadd)
+            print("Updated! Press 1 to view")
+        elif option == "s":
+            several = input("What items would you like to add? Separate your items with a space. ")
+            var = several.split()
+            shoppingList.append(var)
+            print(var)
+            print("Updated! Press 1 to view")
+
+    elif answer == 3:
+        print(shoppingList)
+        item = input("Which item do you choose? Type in the number")
+        if item == shoppingList.index(itemadd):
+            print("I got in!")
+            shoppingList.remove(item)
+            print("Updated! Press 1 to view")
+
+
+    elif answer == 4:
+        print("Thank you for using Shopping List! See you next time!")
+        break
